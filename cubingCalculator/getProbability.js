5fc68c7bff44417cbdf8f70795549e35b47b2e4e
Sethyboy0 <sethyboy0@hotmail.com>
2022-11-11 09:31:24 -0800

    Rename stats to be more specific
    Will be useful for refactoring

--- cubingCalculator/getProbability.js
+++ cubingCalculator/getProbability.js
@@ -3,19 +3,19 @@
 
 // These are all the possible kinds of input the probability calculator can look for.
 export const emptyInputObject = {
-    stat: 0, // At least this much % stat including % allstat lines.
+    percStat: 0, // At least this much % stat including % allstat lines.
     lineStat: 0, // At least this many lines of % stat including allstat lines.
     allStat: 0, // At least this much % all stat including 1/3rd of % STR, DEX, and LUK. For Xenons.
-    hp: 0, // At least this much % HP. For DA.
+    percHp: 0, // At least this much % HP. For DA.
     lineHp: 0, // At least this many lines of % HP. For DA.
-    atk: 0,// At least this much % atk.
+    percAtk: 0,// At least this much % atk.
     lineAtk: 0, // At least this many lines of % atk.
-    boss: 0,
+    percBoss: 0,
     lineBoss: 0,
     lineIed: 0,
     lineCritDamage: 0,
     lineMeso: 0,
     lineDrop: 0,
     lineMesoOrDrop: 0, // At least this many lines of meso OR drop.
-    cooldown: 0, // At least this many seconds of cooldown reduction.
+    secCooldown: 0, // At least this many seconds of cooldown reduction.
 }
