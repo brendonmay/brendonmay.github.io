ed93979d7cb47074ad8326606aa7fdbbbf75fe24
Sethyboy0 <sethyboy0@hotmail.com>
2022-11-25 12:52:28 -0800

    Add QoL function for currentTier

--- cubingCalculator/updateOtherElements.js
+++ cubingCalculator/updateOtherElements.js
@@ -1,11 +1,18 @@
-// Update desired tier when the user changes current tier to something above it.
-// Since you can't tier down, they'll have to change it regardless.
+// These functions update options when the user selects something else incompatible to save a few clicks.
 function updateDesiredTier(currentTier) {
     const $desiredTier = $('#desiredTier');
-
     const currentDesiredTier = parseInt($desiredTier.val());
     if (currentDesiredTier < currentTier) {
         // This doesn't trigger change handlers which is nice.
         $desiredTier.val("" + currentTier);
+    }
+}
+
+function updateCurrentTier(desiredTier) {
+    const $currentTier = $('#currentTier');
+    const currentTierVal = parseInt($currentTier.val());
+    if (currentTierVal > desiredTier) {
+        // This doesn't trigger change handlers which is nice.
+        $currentTier.val("" + desiredTier);
     }
 }
