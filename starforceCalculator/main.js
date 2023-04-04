ba8470e36611cbcfac410889b681e1dd0e410b06
Sethyboy0 <sethyboy0@hotmail.com>
2023-04-04 11:46:49 -0700

    Move server differences to new file

--- starforceCalculator/main.js
+++ starforceCalculator/main.js
@@ -80,57 +80,6 @@ function median(values) {
         return (values[half - 1] + values[half]) / 2.0;
 }
 
-//
-// Server cost functions
-// Values taken from https://strategywiki.org/wiki/MapleStory/Spell_Trace_and_Star_Force#Meso_Cost
-//
-
-function kmsCost(current_star, item_level) {
-    if (current_star >= 15) {
-        return 1000 + item_level ** 3 * ((current_star + 1) ** 2.7) / 200;
-    }
-    if (current_star >= 10) {
-        return 1000 + item_level ** 3 * ((current_star + 1) ** 2.7) / 400;
-    }
-    return 1000 + item_level ** 3 * (current_star + 1) / 25;
-}
-
-function tmsRegCost(current_star, item_level) {
-    if (current_star >= 20) {
-        return 1000 + item_level ** 3 * ((current_star + 1) ** 2.7) / 40;
-    }
-    if (current_star >= 15) {
-        return 1000 + item_level ** 3 * ((current_star + 1) ** 2.7) / 50;
-    }
-    if (current_star >= 11) {
-        return 1000 + item_level ** 3 * ((current_star + 1) ** 2.7) / 66.66;
-    }
-    if (current_star >= 10) {
-        return 1000 + item_level ** 3 * ((current_star + 1) ** 2.7) / 200;
-    }
-    return 1000 + item_level ** 3 * (current_star + 1) / 25;
-}
-
-function tmsRebootCost(current_star, item_level) {
-    const adjusted_level = item_level > 150 ? 150 : item_level;
-    return kmsCost(current_star, adjusted_level);
-}
-
-// Map from server input value to the associated cost function.
-// As of the ignition update GMS uses KMS starforce prices.
-const SERVER_COST_FUNCTIONS = {
-    "kms": kmsCost,
-    "tms": tmsRegCost,
-    "tmsr": tmsRebootCost,
-}
-
-function getBaseCost(server, current_star, item_level) {
-    const costFn = SERVER_COST_FUNCTIONS[server];
-    const attempt_cost = costFn(current_star, item_level);
-    // The game rounds to the nearest 100.
-    return Math.round(attempt_cost / 100) * 100;
-}
-
 function attemptCost(current_star, item_level, boom_protect, thirty_off, sauna, silver, gold, diamond, five_ten_fifteen, chance_time, item_type, server) {
     if (item_type == "tyrant"){
         var attempt_cost = item_level**3.56;
